c6ed2c4fc88a1e8fa5bad550b31132400f9c624d
YiroYi <yiroyi@live.com>
2021-03-28 20:43:47 -0600

    Add: 404 Page for router

--- app.js
+++ app.js
@@ -11,4 +11,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(adminRoutes);
 app.use(shopRoutes);
 
+app.use((req, res, next) => {
+  res.status(404).send('<h1>Page not Found</h1>');
+});
+
 app.listen(3000);
