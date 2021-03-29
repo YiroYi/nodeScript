b04b31a4571a6574c6c2979e74c982766879161c
YiroYi <yiroyi@live.com>
2021-03-28 20:13:53 -0600

    Refactor: app.js file:

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log('Im Yiro');
+  next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log('Im Kripto');
+  res.send('<h1>Im Kripto</h1>');
+});
+
+app.listen(3000);
