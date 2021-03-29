7083c35632a88211871e9ddb8572c85b85b657a2
YiroYi <yiroyi@live.com>
2021-03-28 20:27:48 -0600

    Add: input & routes

--- app.js
+++ app.js
@@ -2,13 +2,22 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('Im Yiro');
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/',(req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  console.log('Im Kripto');
+app.use('/add-product',(req, res, next) => {
+  res.send('<form action="/product" method="POST"><input type="text" name="title"><button>Add product</button></form>');
+});
+
+app.use('/product',(req, res, next) => {
+  console.log(req.body);
+  res.redirect('/');
+});
+
+app.use('/',(req, res, next) => {
   res.send('<h1>Im Kripto</h1>');
 });
 
