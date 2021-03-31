5c004331d0e5fbe91d1aba60aa520484aae4b673
Yiro Yi <yiroyi@live.com>
2021-03-31 00:35:30 -0600

    Revert "Adding/handlebars"

--- routes/shop.js
+++ routes/shop.js
@@ -12,14 +12,7 @@ router.get('/',(req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
   const products = adminData.products;
-  res.render('shop', {pageTitle: 'Shop',
-                      prods: products,
-                      docTitle: 'Shop',
-                      path: '/',
-                      hasProducts: products.length > 0,
-                      activeShop: true,
-                      productCSS: true
-                    });
+  res.render('shop', {prods: products, docTitle: 'Shop', path: '/'});
 });
 
 module.exports = router;
