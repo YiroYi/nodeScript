3b85cec7af816e46aeca5d00462df46ad90e3404
YiroYi <yiroyi@live.com>
2021-03-31 00:38:33 -0600

    Merging to master

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.set('view engine', 'pug');
-
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const path = require('path');
@@ -20,7 +19,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
